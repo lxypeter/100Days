2f15a24aae14d1cc2691fa1762ff488a3015fffe
CY.Lee <lxypeter@sina.com>
2016-10-17 19:39:11 +0800

    update left menu icon

---
+++ CYBaseProject/Classes/Index/View/LeftMenuViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LeftMenuViewCell.h
+//  100Days
+//
+//  Created by Peter Lee on 2016/10/17.
+//  Copyright © 2016年 CY.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftMenuViewCell : UITableViewCell
+
+@end
---
+++ CYBaseProject/Classes/Index/View/LeftMenuViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LeftMenuViewCell.m
+//  100Days
+//
+//  Created by Peter Lee on 2016/10/17.
+//  Copyright © 2016年 CY.Lee. All rights reserved.
+//
+
+#import "LeftMenuViewCell.h"
+
+@implementation LeftMenuViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
